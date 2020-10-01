9bcfc6b66761e59632134c2fdb50db2e18afd945
Sergey Levhuk <levcha.chai@gmail.com>
2020-10-01 22:12:56 +0300

    feat: add change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let avg_value = (arr) => arr.reduce((a, b) => a + b, 0) / array.length;
+    return avg_value(array);
+};
